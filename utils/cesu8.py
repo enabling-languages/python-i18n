9b6bb3bc368a85dedd48f6c9947e4f6f4def9027
andjc <ajcunningham@deaking.edu.au>
2021-09-20 13:35:19 +1000

    Includes the following updates:
    * added MARC21 Nomrlaisation support
    * added CESU-8 codec
    * Updated DUCET allkeys file to Unicode 14

---
+++ utils/cesu8.py
@@ -0,0 +1,155 @@
+# Copyright 2014, 2015 SAP SE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#################################################################################
+#
+# Source for CESU-8 codec: https://github.com/SAP-archive/PyHDB/blob/master/pyhdb/cesu8.py
+#
+# Changes made by Enabling Languages (09/2021):
+#     * Removed Python2 support.
+
+import codecs
+
+SURROGATE_IDENTICATOR_INT = 0xED
+SURROGATE_IDENTICATOR_BYTE = b'\xed'
+
+class IncrementalDecoder(codecs.BufferedIncrementalDecoder):
+    # Decoder inspired by python-ftfy written by Rob Speer
+    # https://github.com/LuminosoInsight/python-ftfy/blob/master/ftfy/bad_codecs/utf8_variants.py
+
+    def _buffer_decode(self, input, errors, final):
+        decoded_segments = []
+        position = 0
+
+        while True:
+            decoded, consumed = self._buffer_decode_step(input[position:], errors, final)
+
+            if consumed == 0:
+                break
+
+            decoded_segments.append(decoded)
+            position += consumed
+
+        if final and position != len(input):
+            raise Exception("Final decoder doesn't decoded all bytes")
+
+        return u''.join(decoded_segments), position
+
+    def _buffer_decode_step(self, input, errors, final):
+        # If begin of CESU-8 sequence
+        if input.startswith(SURROGATE_IDENTICATOR_BYTE):
+            if len(input) < 6:
+                if not final:
+                    # Stream is not done yet
+                    return u'', 0
+
+                # As there are less than six bytes it can't be a CESU-8 surrogate
+                # but probably a UTF-8 byte sequence
+                return codecs.utf_8_decode(input, errors, final)
+
+            bytenums = input
+
+            # Verify that the 6 bytes are in possible range of a CESU-8 surrogate
+            if bytenums[1] >= 0xa0 and bytenums[1] <= 0xbf and \
+               bytenums[2] >= 0x80 and bytenums[2] <= 0xbf and \
+               bytenums[3] == SURROGATE_IDENTICATOR_INT and \
+               bytenums[4] >= 0xb0 and bytenums[4] <= 0xbf and \
+               bytenums[5] >= 0x80 and bytenums[5] <= 0xbf:
+
+                codepoint = (
+                    ((bytenums[1] & 0x0f) << 16) +
+                    ((bytenums[2] & 0x3f) << 10) +
+                    ((bytenums[4] & 0x0f) << 6) +
+                    (bytenums[5] & 0x3f) +
+                    0x10000
+                )
+                return chr(codepoint), 6
+
+            # No CESU-8 surrogate but probably a 3 byte UTF-8 sequence
+            return codecs.utf_8_decode(input[:3], errors, final)
+
+        cesu8_surrogate_start = input.find(SURROGATE_IDENTICATOR_BYTE)
+        if cesu8_surrogate_start > 0:
+            # Decode everything until start of cesu8 surrogate pair
+            return codecs.utf_8_decode(input[:cesu8_surrogate_start], errors, final)
+
+        # No sign of CESU-8 encoding
+        return codecs.utf_8_decode(input, errors, final)
+
+class IncrementalEncoder(codecs.BufferedIncrementalEncoder):
+
+    def _buffer_encode(self, input, errors, final=False):
+        encoded_segments = []
+        position = 0
+        input_length = len(input)
+
+        while position + 1 <= input_length:
+            encoded, consumed = self._buffer_encode_step(
+                input[position], errors, final
+            )
+
+            if consumed == 0:
+                break
+
+            encoded_segments.append(encoded)
+            position += consumed
+
+        if final and position != len(input):
+            raise Exception("Final encoder doesn't encode all characters")
+
+        return b''.join(encoded_segments), position
+
+    def _buffer_encode_step(self, char, errors, final):
+        codepoint = ord(char)
+        if codepoint <= 65535:
+            return codecs.utf_8_encode(char, errors)
+        else:
+            seq = bytearray(6)
+            seq[0] = 0xED
+            seq[1] = 0xA0 | (((codepoint & 0x1F0000) >> 16) - 1)
+            seq[2] = 0x80 | (codepoint & 0xFC00) >> 10
+            seq[3] = 0xED
+            seq[4] = 0xB0 | ((codepoint >> 6) & 0x3F)
+            seq[5] = 0x80 | (codepoint & 0x3F)
+            return bytes(seq), 1
+
+def encode(input, errors='strict'):
+    return IncrementalEncoder(errors).encode(input, final=True), len(input)
+
+def decode(input, errors='strict'):
+    return IncrementalDecoder(errors).decode(input, final=True), len(input)
+
+class StreamWriter(codecs.StreamWriter):
+    encode = encode
+
+class StreamReader(codecs.StreamReader):
+    decode = decode
+
+CESU8_CODEC_INFO = codecs.CodecInfo(
+    name="cesu-8",
+    encode=encode,
+    decode=decode,
+    incrementalencoder=IncrementalEncoder,
+    incrementaldecoder=IncrementalDecoder,
+    streamreader=StreamReader,
+    streamwriter=StreamWriter,
+)
+
+def search_function(encoding):
+    if encoding == 'cesu-8':
+        return CESU8_CODEC_INFO
+    else:
+        return None
+
+codecs.register(search_function)
--- utils/el_utils.py
+++ utils/el_utils.py
@@ -1,14 +1,31 @@
-#
+#!/usr/bin/env python3
+
+# #
 # EL Util
-#    various helper functions
+#    various helper functions for internal Enabling Language projects
 #
 #    Copyright © 2021 Enabling Languages.
-#    This notebook is made available under the MIT licence.
+#    This file is made available under the MIT licence.
 #
 
-import unicodedataplus as ud
+import os, sys
+try:
+    import unicodedataplus as ud
+except ImportError:
+    import unicodedata as ud
+try:
+    import regex as re
+except ImportError:
+    import re
+import codecs
 from tabulate import tabulate
 import grapheme
+
+# Code for interbnal testing and development:
+#libpath = os.path.expanduser('~/dev/i18n/py/utils')
+#if libpath not in sys.path:
+#    sys.path.append(libpath)
+#import cesu8
 
 # Typecast string to a list, splitting characters
 def splitString(text):
@@ -20,9 +37,50 @@ def utf8len(text):
 def utf16len(text):
     return len(text.encode('utf-16-le'))
 
+# Replace values matching dictionary keys with values
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
+
+# Normalise strings according to MARC21 Character repetoire requirements
+# MNF (Marc Normalisation Form)
+def marc21_normalise(text):
+    # Normalise to NFD
+    text = normalise(text, "NFD")
+    # Latin variations between NFD and MNF
+    latn_rep = {
+    "\u004F\u031B": "Ơ",
+    "\u008F\u031B": "ơ",
+    "\u0055\u031B": "Ư",
+    "\u0075\u031B": "ư"
+    }
+    # Cyrillic variations between NFD and MNF
+    cyrl_rep = {
+        "\u0413\u0301": "Ѓ",
+        "\u0433\u0301": "ѓ",
+        "\u0415\u0308": "Ё",
+        "\u0435\u0308": "ё",
+        "\u0406\u0308": "Ї",
+        "\u0456\u0308": "ї",
+        "\u041A\u0301": "Ќ",
+        "\u043A\u0301": "ќ",
+        "\u0423\u0306": "Ў",
+        "\u0443\u0306": "ў"
+    }
+    # Only process strings containing charcaters that need chasing
+    if bool(re.search(r'[ouOU]\u031B', text)):
+        text = replace_all(text, latn_rep)
+    if bool(re.search(r'[\\u0413\u041Au0433\u043A]\u0301|[\u0406\u0415\u0435\u0456]\u0308|[\u041A\u043A]\u0301|[\u0423\u0443]\u0306', text)):
+        text = replace_all(text, cyrl_rep)
+    return text
+
 # Normalise to specified Unicode Normalisation Form, defaulting to NFC.
+# nf = NFC | NFKC | NFD | NFKD | MNF
 def normalise(text, nf="NFC"):
     nf = nf.upper()
+    if nf == "MNF":
+        return marc21_normalise(text)
     if not (ud.is_normalized(nf, text)):
         return ud.normalize(nf, text)
     return text
@@ -33,6 +91,7 @@ def codepoints(text):
 
 # table of codepoints in string, giving basic data on each charcater
 def udata(text):
+    print("String: " + text)
     data = []
     datahead = ["char", "cp", "name", "script", "block", "cat", "bidi"]
     for c in splitString(text):
@@ -52,3 +111,179 @@ def stringLength(text):
     ]
     datahead = ['Component', 'Count']
     print(tabulate(data, headers=datahead, tablefmt='grid'))
+
+# Convert a string of comma or space seperated unicode codepoints to characters
+#    Usage: codepointsToChar("U+0063 U+0301")
+#        or codepointsToChar("0063 0301")
+def codepointsToChar(str):
+    str = str.lower().replace("u+", "")
+    l = re.split(", |,| ", str)
+    #l = str.lower().replace("u+", "").split(" ")
+    r = ""
+    for c in l:
+        r += chr(int(c, 16))
+    return r
+
+# Get unicode codepoints for surrogate pairs for charcaters in SMP
+#    Usage: surrogate_pair("𞤁")
+def surrogatePair(c):
+    if ord(c) > 0xffff:
+        s1 = "".join(["{:02x}".format(x) for x in bytes(c, 'utf-16-le')[0:2]]).upper()
+        s2 = "".join(["{:02x}".format(x) for x in bytes(c, 'utf-16-le')[2:4]]).upper()
+        return "U+" + s1 + " U+" + s2
+    return ""
+
+# DEPRECATED: Use CESU-8 codec instead
+def oracle_utf8(c):
+    if ord(c) > 0xffff:
+        s1 = "".join(["{:02x}".format(x) for x in bytes(c, 'utf-16-be')[0:2].decode('utf-16-be', 'surrogatepass').encode('utf-8', 'surrogatepass')]).upper()
+        s2 = "".join(["{:02x}".format(x) for x in bytes(c, 'utf-16-be')[2:4].decode('utf-16-be', 'surrogatepass').encode('utf-8', 'surrogatepass')]).upper()
+        return s1 + " " + s2
+    return ""
+
+# DEPRECATED: Use CESU-8 codec instead
+def uni_bytes_oracle_utf8(c):
+    if ord(c) > 0xffff:
+        b1 = ["{:02x}".format(x) for x in bytes(c, 'utf-16-be')[0:2].decode('utf-16-be', 'surrogatepass').encode('utf-8', 'surrogatepass')]
+        b2 = ["{:02x}".format(x) for x in bytes(c, 'utf-16-be')[2:4].decode('utf-16-be', 'surrogatepass').encode('utf-8', 'surrogatepass')]
+        return b1 + b2
+    return []
+
+
+# Generate a table conatining byte sequences representing specified character in each Unicode encoding
+#   Usage: byteSequences("𞤁")
+def byteSequences(c, additional=[]):
+    print("Character: " + c)
+    print("Codepoint: " + "U+"+"%04X"%(ord(c)))
+    if ord(c) > 0xffff:
+        print("Surrogate pair: " + surrogatePair(c))
+    data = [
+        ['utf-8', " ".join(["{:02x}".format(x) for x in bytes(c, 'utf-8')]).upper()], 
+        ['utf-16-le'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-16-le')]).upper()], 
+        ['utf-16-be'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-16-be')]).upper()], 
+        ['utf-32-le'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-32-le')]).upper()], 
+        ['utf-32-be'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-32-be')]).upper()]
+    ]
+    #if additional.lower() == "cesu-8":
+    if "cesu-8" in additional:
+        data.append(['cesu-8', " ".join(["{:02x}".format(x) for x in codecs.encode(s, 'cesu-8')]).upper()] )
+    datahead = ['Encoding', 'Byte sequence']
+    print(tabulate(data, headers=datahead, tablefmt='grid'))
+
+# Get list of unicode codepoints in a string
+#    Usage: uni_cp("ꕙꔤ")
+#           uni_cp("ꕙꔤ", False)
+def uni_cp(txt, prefix=True):
+    #return ["U+"+"%04X"%(ord(c)) for c in txt] if prefix else ["%04X"%(ord(c)) for c in txt]
+    return ['U+{:04X}'.format(ord(c)) for c in txt] if prefix else ['{:04X}'.format(ord(c)) for c in txt]
+
+# Get list of bytes that represent the string
+#    Usage: uni_bytes("ꕙꔤ")
+#           uni_bytes("ꕙꔤ", "utf-16-le")
+def uni_bytes(txt, enc="utf-8"):
+    enc = enc.lower()
+    print(enc)
+    #if mode.lower() =="oracle":
+    #    return  uni_bytes_oracle_utf8(txt)
+    if enc == "utf-8":
+        return ["{:02x}".format(x) for x in bytes(txt, 'utf-8')]
+    elif enc == "utf-16-le" or enc == "utf-16le":
+        return ["{:02x}".format(x) for x in bytes(txt, 'utf-16-le')]
+    elif enc == "utf-16-be" or enc == "utf-16be":
+        return ["{:02x}".format(x) for x in bytes(txt, 'utf-16-be')]
+    elif enc == "utf-32-le" or enc == "utf-32le":
+        return ["{:02x}".format(x) for x in bytes(txt, 'utf-32-le')]
+    elif enc == "utf-32-be" or enc == "utf-32be":
+        return ["{:02x}".format(x) for x in bytes(txt, 'utf-32-be')]
+    elif enc == "cesu-8":
+        return ["{:02x}".format(x) for x in codecs.encode(txt, 'cesu-8')]
+    return ""
+
+# Force escaped surrogate pairs to be convert ro characters
+#    Usage: surrogatesToChar("\u3AD8\u01DD")
+def surrogatesToChar(seq):
+    return seq.encode('utf-16', 'surrogatepass').decode('utf-16')
+
+
+####################
+#
+#  TODO:
+#
+####################
+#
+#  1. strip bidi formating characters
+#  2. strip punctuation
+#  3. normalise white space, ie collapse multiple spaces to a single space, 
+#     trim spaces at beginning of line or at end of line
+#
+
+def bidi_envelope(s, dir="auto", mode="isolate"):
+    mode = mode.lower()
+    dir = dir.lower()
+    if mode == "isolate":
+        if dir == "rtl":
+            s = "\u2067" + s + "\u2069"
+        elif dir == "ltr":
+            s = "\u2066" + s + "\u2069"
+        elif dir == "auto":
+            s = "\u2068" + s + "\u2069"
+    elif mode == "embedding":
+        if dir == "auto":
+            dir = "rtl"
+        if dir == "rtl":
+            s = "\u202B" + s + "\u202C"
+        elif dir == "ltr":
+            s = "\u202A" + s + "\u202C"
+    return s
+
+
+# strip bidi formating characters
+def strip_bidi(s):
+    # U+2066..U+2069, U+202A..U+202E
+    return re.sub('[\u202a-\u202e\u2066-\u2069]', '', s)
+
+# strip punctuation
+def strip_punct(s, include=""):
+    if include == "hyphen":
+        #s = re.sub('\p{P}(?<!-)', '', s)
+        #s = re.sub('\p{P}(?<!((\w-|-\w)))', '', s)
+        s = re.sub('\p{P}(?<!(([^\b\s\p{P}]-)))', '', s)
+        s = re.sub('-(?=[\s\b\p{P}])', '', s)
+    elif include == "quote":
+        #s = re.sub('\p{P}(?<!\')', '', s)
+        #s = re.sub('\p{P}(?<!((\w\'|\'\w)))', '', s)
+        s = re.sub('-(?=[\w])', ' ', s)
+        s = re.sub('\p{P}(?<!(([^\b\s\p{P}]\')))', '', s)
+        s = re.sub('\'(?=[\s\b\p{P}])', '', s)
+    elif include == "both":
+        #s = re.sub('\p{P}(?<![-\'])', '', s)
+        #s = re.sub('\p{P}(?<!((\w[-\']|[-\']\w)))', '', s)
+        s = re.sub('\p{P}(?<!(([^\b\s\p{P}][\'-])))', '', s)
+        s = re.sub('[\'-](?=[\s\b\p{P}])', '', s)
+    else:
+        s = re.sub('-(?=[\w])', ' ', s)
+        s = re.sub('\p{P}', '', s)
+    return s
+# test string 
+#  \t\n  'Once upon'ga time, help; - trans-from here's 78 'the rub'?! 'Blah, blah' yeah.'    \t\n  
+# 
+
+def text_sanitise(s):
+    s = ud.normalize("NFC", s)
+    s = strip_bidi(s)
+    s = strip_punct(s, include="both")
+    s = s.strip()
+    return s
+
+# import el_utils as elu
+# s = "𞤁𞤫𞤬𞤼𞤫𞤪𞤫 𞤨𞤢𞤴𞤳𞤮𞤴."
+# print(elu.bidi_envelope(s, "rtl"))
+# print(elu.strip_bidi(elu.bidi_envelope(s, "rtl")))
+
+# macOS:
+#    * Terminal - bidi support, but in LTR environment. Isolate and Ebedding modes work as epected.
+#    * iTerm2 - no bidi support
+#    * vsc integrated terminal - no bidi support
+
+
+
