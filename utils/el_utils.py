66bf9f436e89cdadc1c57ab1bb80197f9479fd1f
Andj <andjc@users.noreply.github.com>
2022-03-12 15:09:08 +1100

    Updates to el_utls.py

--- utils/el_utils.py
+++ utils/el_utils.py
@@ -4,28 +4,38 @@
 # EL Util
 #    various helper functions for internal Enabling Language projects
 #
-#    Copyright © 2021 Enabling Languages.
+#    Copyright © 2021-2022 Enabling Languages.
 #    This file is made available under the MIT licence.
 #
+# Usage:
+#    import os, sys
+#    libpath = os.path.expanduser('~/dev/i18n/py/utils/')
+#    if libpath not in sys.path:
+#        sys.path.append(libpath)
+#    import el_transliteration
+#    import el_utils as elu
 
 import os, sys
-try:
-    import unicodedataplus as ud
-except ImportError:
-    import unicodedata as ud
-try:
-    import regex as re
-except ImportError:
-    import re
+import unicodedataplus as ud
+import regex as re
 import codecs
 from tabulate import tabulate
 import grapheme
-
-# Code for interbnal testing and development:
-#libpath = os.path.expanduser('~/dev/i18n/py/utils')
-#if libpath not in sys.path:
-#    sys.path.append(libpath)
+from laonlp.tokenize import word_tokenize as lao_wt
+from pythainlp.tokenize import word_tokenize as thai_wt
+#from khmernltk import word_tokenize as khmer_wt
+from icu import BreakIterator, Locale, UnicodeString, Transliterator, UTransDirection
+# Code for internal testing and development:
+# libpath = os.path.expanduser('~/dev/i18n/libr/yale-lao')
+libpath = os.path.expanduser('./')
+if libpath not in sys.path:
+    sys.path.append(libpath)
 #import cesu8
+#from el_transliteration import transliteration_data
+
+ROOT_LOCALE = Locale.getRoot()
+# DEFAULT_LOCALE = Locale("en-AU.UTF8")
+DEFAULT_LOCALE = ROOT_LOCALE
 
 # Typecast string to a list, splitting characters
 def splitString(text):
@@ -47,7 +57,7 @@ def replace_all(text, dic):
 # MNF (Marc Normalisation Form)
 def marc21_normalise(text):
     # Normalise to NFD
-    text = normalise(text, "NFD")
+    text = normalise("NFD", text)
     # Latin variations between NFD and MNF
     latn_rep = {
     "\u004F\u031B": "Ơ",
@@ -73,27 +83,56 @@ def marc21_normalise(text):
     # Only process strings containing charcaters that need chasing
     if bool(re.search(r'[ouOU]\u031B', text)):
         text = replace_all(text, latn_rep)
-    if bool(re.search(r'[\u0413\u041A\u0433\u043A]\u0301|[\u0418\u0423\u0438\u0443]\u0306|[\u0406\u0415\u0435\u0456]\u0308', text))
+    if bool(re.search(r'[\u0413\u041A\u0433\u043A]\u0301|[\u0418\u0423\u0438\u0443]\u0306|[\u0406\u0415\u0435\u0456]\u0308', text)):
         text = replace_all(text, cyrl_rep)
     return text
 
 # Normalise to specified Unicode Normalisation Form, defaulting to NFC.
-# nf = NFC | NFKC | NFD | NFKD | MNF
-def normalise(text, nf="NFC"):
+# nf = NFC | NFKC | NFD | NFKD | NFM
+def normalise(nf, s):
     nf = nf.upper()
-    if nf == "MNF":
-        return marc21_normalise(text)
-    if not (ud.is_normalized(nf, text)):
-        return ud.normalize(nf, text)
-    return text
+    if nf == "NFM":
+        return marc21_normalise(s)
+    else:
+        return ud.normalize(nf, s)
+    return s
 
-# List of codepoints in string
-def codepoints(text):
-    return ' '.join('U+{:04X}'.format(ord(c)) for c in text)
+# codepoints in string
+# def codepoints(text, prefix=True):
+#     return ' '.join('U+{:04X}'.format(ord(c)) for c in text) if prefix else ' '.join('{:04X}'.format(ord(c)) for c in text)
 
-# List codepoints and characters in string
-def cp(text):
-    return ' '.join(f"U+{ord(c):04X} ({c})" for c in text)
+# codepoints and characters in string
+#
+# Usage:
+#    elu.codepoints("𞤀𞤣𞤤𞤢𞤥 𞤆𞤵𞤤𞤢𞤪")
+#    elu.cp("𞤀𞤣𞤤𞤢𞤥 𞤆𞤵𞤤𞤢𞤪", extended=False)
+#    elu.cp("𞤀𞤣𞤤𞤢𞤥 𞤆𞤵𞤤𞤢𞤪", prefix=False, extended=False)
+
+def codepoints(text, prefix=True, extended=True):
+    if extended:
+        return ' '.join(f"U+{ord(c):04X} ({c})" for c in text) if prefix else ' '.join(f"{ord(c):04X} ({c})" for c in text)
+    else:
+        return ' '.join('U+{:04X}'.format(ord(c)) for c in text) if prefix else ' '.join('{:04X}'.format(ord(c)) for c in text)
+
+cp = codepoints
+
+def graphemes(text):
+    return re.findall(r'\X',text)
+
+gr = graphemes
+
+def isalpha_(text):
+    if len(text) == 1:
+        result = bool(re.match(r'[\p{Alphabetic}\p{Mn}\p{Mc}·]', text))
+    else:
+        result = bool(re.match(r'^\p{Alphabetic}[\p{Alphabetic}\p{Mn}\p{Mc}·]*$', text))
+    # Add Mn and Mc categories to Unicode Alphabetic derived property
+    # https://util.unicode.org/UnicodeJsps/list-unicodeset.jsp
+    return result
+
+def isalpha_unicode(text):
+    # Unicode Alphabetic derived property
+    return bool(re.match(r'^\p{Alphabetic}+$', text))
 
 # table of codepoints in string, giving basic data on each charcater
 def udata(text):
@@ -109,10 +148,10 @@ def stringLength(text):
     print("String: " + text)
     print("Codepoints: " +  codepoints(text))
     data = [
-        ['Characters', len(text)], 
-        ['Bytes', utf8len(text)], 
+        ['Characters', len(text)],
+        ['Bytes', utf8len(text)],
         ['Graphemes', grapheme.length(text)],
-        ['Syllables', ""], 
+        ['Syllables', ""],
         ['Words', ""]
     ]
     datahead = ['Component', 'Count']
@@ -130,7 +169,7 @@ def codepointsToChar(str):
         r += chr(int(c, 16))
     return r
 
-# Get unicode codepoints for surrogate pairs for charcaters in SMP
+# Get unicode codepoints for surrogate pairs for characters in SMP and SIP
 #    Usage: surrogate_pair("𞤁")
 def surrogatePair(c):
     if ord(c) > 0xffff:
@@ -164,17 +203,18 @@ def byteSequences(c, additional=[]):
     if ord(c) > 0xffff:
         print("Surrogate pair: " + surrogatePair(c))
     data = [
-        ['utf-8', " ".join(["{:02x}".format(x) for x in bytes(c, 'utf-8')]).upper()], 
-        ['utf-16-le'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-16-le')]).upper()], 
-        ['utf-16-be'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-16-be')]).upper()], 
-        ['utf-32-le'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-32-le')]).upper()], 
+        ['utf-8', " ".join(["{:02x}".format(x) for x in bytes(c, 'utf-8')]).upper()],
+        ['utf-16-le'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-16-le')]).upper()],
+        ['utf-16-be'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-16-be')]).upper()],
+        ['utf-32-le'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-32-le')]).upper()],
         ['utf-32-be'," ".join(["{:02x}".format(x) for x in bytes(c, 'utf-32-be')]).upper()]
     ]
     #if additional.lower() == "cesu-8":
     if "cesu-8" in additional:
-        data.append(['cesu-8', " ".join(["{:02x}".format(x) for x in codecs.encode(s, 'cesu-8')]).upper()] )
+        data.append(['cesu-8', " ".join(["{:02x}".format(x) for x in codecs.encode(c, 'cesu-8')]).upper()] )
     datahead = ['Encoding', 'Byte sequence']
     print(tabulate(data, headers=datahead, tablefmt='grid'))
+    return
 
 # Get list of unicode codepoints in a string
 #    Usage: uni_cp("ꕙꔤ")
@@ -213,16 +253,25 @@ def surrogatesToChar(seq):
 
 ####################
 #
-#  TODO:
+#  Bidi
 #
 ####################
-#
+#  TODO:
 #  1. strip bidi formating characters
 #  2. strip punctuation
-#  3. normalise white space, ie collapse multiple spaces to a single space, 
+#  3. normalise white space, ie collapse multiple spaces to a single space,
 #     trim spaces at beginning of line or at end of line
 #
 
+def is_bidi(s):
+    bidi_reg = r'[\p{bc=AL}\p{bc=AN}\p{bc=LRE}\p{bc=RLE}\p{bc=LRO}\p{bc=RLO}\p{bc=PDF}\p{bc=FSI}\p{bc=RLI}\p{bc=LRI}\p{bc=PDI}\p{bc=R}]'
+    return bool(re.search(bidi_reg, s))
+
+isbidi = is_bidi
+
+# Wrap string in bidirection formatting characters
+#    dir = ltr, rtl or auto
+#    mode = isolate or embedding
 def bidi_envelope(s, dir="auto", mode="isolate"):
     mode = mode.lower()
     dir = dir.lower()
@@ -240,8 +289,14 @@ def bidi_envelope(s, dir="auto", mode="isolate"):
             s = "\u202B" + s + "\u202C"
         elif dir == "ltr":
             s = "\u202A" + s + "\u202C"
+    elif mode == "override":
+        if dir == "auto":
+            dir = "rtl"
+        if dir == "rtl":
+            s = "\u202E" + s + "\u202C"
+        elif dir == "ltr":
+            s = "\u202D" + s + "\u202C"
     return s
-
 
 # strip bidi formating characters
 def strip_bidi(s):
@@ -270,9 +325,9 @@ def strip_punct(s, include=""):
         s = re.sub('-(?=[\w])', ' ', s)
         s = re.sub('\p{P}', '', s)
     return s
-# test string 
-#  \t\n  'Once upon'ga time, help; - trans-from here's 78 'the rub'?! 'Blah, blah' yeah.'    \t\n  
-# 
+# test string
+#  \t\n  'Once upon'ga time, help; - trans-from here's 78 'the rub'?! 'Blah, blah' yeah.'    \t\n
+#
 
 def text_sanitise(s):
     s = ud.normalize("NFC", s)
@@ -291,5 +346,233 @@ def text_sanitise(s):
 #    * iTerm2 - no bidi support
 #    * vsc integrated terminal - no bidi support
 
+####################
+#
+#  Bibliographic data
+#
+####################
+#  TODO:
+#  1. add additional segmentation engines ofr Thai, Khmer, Myanmar and Tibetan
+#  2. refactor transliteration
+#     a. making a reversible transliteration wrapper
+#     b. define rules for ALA-LC romanisation tables
+#     b. add additional language dictionaries
+#
 
+#
+# Word segmentation
+#
 
+# SUPPORTED_LANGUAGES = ['bo', 'bo_CN', 'bo_IN', 'km', 'km_KH', 'lo', 'lo_LA', 'my', 'my_MM', 'th', 'th_TH']
+SUPPORTED_LANGUAGES = list(BreakIterator.getAvailableLocales())
+SUPPORTED_ENGINES = ['icu', 'laonlp', 'thainlp']
+SUPPORTED_SEPERATORS = ['\u0020', '\u007C', '\u200B']
+SUPPORTED_NORMALISATION_FORMS = ['nfc', 'nfkc', 'nfd', 'nfkd', 'nfm']
+
+def laonlp_tokenise(s, sep):
+    s = sep.join(lao_wt(s))
+    s = re.sub(r"\s{2,}", " ", s)
+    return re.sub(r'\s([?.!"](?:\s|$))', r'\1', s)
+
+#def thainlp_tokenise(s, sep):
+#    s = sep.join(thai_wt(s))
+#    s = re.sub(r"\s{2,}", " ", s)
+#    return re.sub(r'\s([?.!"](?:\s|$))', r'\1', s)
+
+def iterate_breaks(text, bi):
+    bi.setText(text)
+    lastpos = 0
+    while True:
+        next_boundary = bi.nextBoundary()
+        if next_boundary == -1: return
+        yield text[lastpos:next_boundary]
+        lastpos = next_boundary
+
+def icu_tokenise(s, l, sep):
+    if l.lower() == "lo":
+        bi = BreakIterator.createWordInstance(Locale('lo_LA'))
+    if l.lower() == "th":
+        bi = BreakIterator.createWordInstance(Locale('th_TH'))
+    s = sep.join(list(iterate_breaks(s, bi)))
+    s = re.sub(r"\s{2,}", " ", s)
+    s = re.sub(r'\s([?.!"](?:\s|$))', r'\1', s)
+    return s
+
+def segment_words(text, engine="icu", lang="", sep="\u0020"):
+    engine = engine.lower()
+    lang = lang.replace("-", "_").split('.')[0]
+    if engine not in SUPPORTED_ENGINES:
+        print("Unsupported tokenisation engine specified", file=sys.stderr)
+        sys.exit(1)
+    if lang not in SUPPORTED_LANGUAGES:
+        print("Unsupported language specified", file=sys.stderr)
+        sys.exit(1)
+    if sep not in SUPPORTED_SEPERATORS:
+        print("Unsupported token seperator", file=sys.stderr)
+        sys.exit(1)
+    if engine == "icu":
+        # return icu_tokenise(text, lang, sep)
+        bi = BreakIterator.createWordInstance(Locale(lang))
+        text = sep.join(list(iterate_breaks(text, bi)))
+        text = re.sub(r"\s{2,}", " ", text)
+        text = re.sub(r'\s([?.!"](?:\s|$))', r'\1', text)
+        return text
+    if engine == "laonlp" and lang[0:2] == "lo":
+        return laonlp_tokenise(text, sep)
+    # if engine == "thainlp" and lang[0:2] == "th":
+    #     return thainlp_tokenise(text, sep)
+
+#
+# Transliteration
+#
+
+DEFAULT_NF = "NFD"
+
+def prep_string(s, dir, lang, b="latin-only"):
+    if dir.lower() == "reverse" and b.lower() != "both":
+        s = s.lower()
+    #s = ud.normalize('NFD', s)
+    s = normalise(DEFAULT_NF, s)
+    if lang == "lo" and dir.lower() == "reverse":
+        s = s.replace("\u0327", "\u0328").replace("\u031C", "\u0328")
+    return s
+
+# direction (dir) = direction of transliteration ; forward (to Latin) | reverse (from Latin)
+# bicameral script (bicameral) = latin_only | both
+# def to_native(bib_data, translit_table="laoo_t_latn_m0_ALALOC", dir="reverse", bicameral="latin_only" ):
+#     bib_data = prep_string(bib_data, dir, bicameral)
+#     word_dict = {}
+#     if translit_table == "laoo_t_latn_m0_ALALOC":
+#         from laoo_t_latn_m0_ALALOC import translit_dict, translit_rules
+#         word_dict = translit_dict[dir]
+#         label = "Lao-Latin/ALALOC"
+#         #custom_transliterator = Transliterator.createFromRules(label, translit_rules, UTransDirection.REVERSE)
+#         #res = " ".join(word_dict.get(ele, ele) for ele in bib_data.split())
+#         bib_data_split = re.split('(\W+?)', bib_data)
+#         res = "".join(word_dict.get(ele, ele) for ele in bib_data_split)
+#         translit_result = res
+#         #translit_result = custom_transliterator.transliterate(res)
+#     else:
+#         translit_result = bib_data
+#     return translit_result
+
+SUPPORTED_TRANSLITERATORS = {
+    "bo": ("", "latin_only", ""),
+    "kh": ("", "latin_only", ""),
+    "lo": ("laoo_t_latn_m0_ALALOC", "latin_only", "Lao-Latin/ALALOC"),
+    "my": ("", "latin_only", ""),
+    "th": ("", "latin_only", "")
+}
+
+def el_transliterate(bib_data, lang, dir="forward", nf="nfd"):
+    lang = lang.replace("-", "_").split('_')[0]
+    dir = dir.lower()
+    if dir != "reverse":
+        dir = "forward"
+    if SUPPORTED_TRANSLITERATORS[lang]:
+        #bib_data = prep_string(bib_data, dir, SUPPORTED_TRANSLITERATORS[lang][1])
+        translit_table = SUPPORTED_TRANSLITERATORS[lang]
+        nf = nf.lower() if nf.lower() in SUPPORTED_NORMALISATION_FORMS else "nfd"
+        bib_data = prep_string(bib_data, dir, lang, translit_table[1])
+        word_dict = transliteration_data[translit_table[0]]['translit_dict'][dir]
+        label = translit_table[2]
+        if dir == "reverse":
+            bib_data_split = re.split('(\W+?)', bib_data)
+            res = "".join(word_dict.get(ele, ele) for ele in bib_data_split)
+        else:
+            pattern = re.compile('|'.join(word_dict.keys()))
+            res = pattern.sub(lambda x: word_dict[x.group()], bib_data)
+            # for key, value in word_dict.items():
+            #    res = bib_data.replace(key, value)
+            # bib_data_split = re.split('(\W+?)', bib_data)
+            # res = "".join(word_dict.get(ele, ele) for ele in bib_data_split)
+            # for key, value in word_dict.items():
+            #     if key in bib_data:
+            #         res = bib_data.replace(key, value)
+
+    else:
+        res = bib_data
+    if nf != "nfd":
+        res = normalise(nf, res)
+    return res
+
+# el_transliterate("vœ̄nvīlāvong", "lo-LA", dir="reverse")
+
+# el_transliterate("vœ̄nvīlāvong", "lo", dir="reverse")
+
+# el_transliterate("vœ̄nvīlāvong", "lo", dir="reverse", nf="nfd")
+
+# test = "laoo_t_latn_m0_ALALOC"
+# dir = "forward"
+# rules_data = transliteration_data[test]['translit_dict'][dir]
+# print(rules_data)
+# ruleset = transliteration_data[test]["translit_rules"]
+# print(ruleset)
+
+#
+# Numbers
+#
+#     * For chinese numbers: pycnnum 1.0.1  - https://pypi.org/project/pycnnum/
+#     * Misc, including ethiopic https://github.com/jameshfisher/numeral
+#     * https://blog.derso.org/recursive-algorithms-for-geez-numerals/
+#          * https://github.com/ebenh/abyssinica/blob/52fa074b8fc0afad729513f56cda5c805212eae1/abyssinica/numerals.py#L124-#L149
+
+# Convert decimal numbers to Arabic digits
+#   s = string of digits
+#   sep is a tuple containing the thousands (grouping) seperator and the decimal seperator
+def convert_digits(s, sep = (",", ".")):
+    nd = re.compile(r'^-?\p{Nd}[,.\u066B\u066C\u0020\u2009\p{Nd}]+$')
+    tsep, dsep = sep
+    if nd.match(s):
+        s = s.replace(tsep, "")
+        s = ''.join([str(ud.decimal(c, c)) for c in s])
+        if dsep in s:
+            s = float(s.replace(dsep, ".")) if dsep != "." else float(s)
+            return s
+        s = int(s.replace(dsep, ".")) if dsep != "." else  int(s)
+    return s
+
+# returns an int or float
+#
+# convert_digits('๓๒.๓')
+# convert_digits("၉၃,၇၀၄.၈")
+# convert_digits("၉၃,၇၀၄")
+# convert_digits("1 234,01", (" ", ","))
+# convert_digits('७,३७,७७,८८,६७३')
+
+def is_number(v, sep = (",", ".")):
+    original = v
+    n = re.compile(r'^-?\p{N}[,.\u066B\u066C\u0020\u2009\p{N}]+$')
+    nd = re.compile(r'^-?\p{Nd}[,.\u066B\u066C\u0020\u2009\p{Nd}]+$')
+    v = "".join(v.split())
+    if isinstance(v, int) or isinstance(v, float):
+        return isinstance(v, (int, str)), type(v), v
+    elif isinstance(v.strip(), str) and nd.match(v.strip()):
+        v = convert_digits(v.strip(), sep)
+        return True, type(v), v
+    else:
+        return False, type(v), v
+
+# Returns tuple of boolean (is or isn't a decimal number), type, and value
+# is_number('๓๒.๓')
+# is_number("၉၃,၇၀၄.၈")
+# is_number("၉၃,၇၀၄")
+# is_number("1 234,01", (" ", ","))
+# is_number('७,३७,७७,८८,६७३')
+
+# ICU casing and casefolding
+#   pass string, case (lower, upper, title, casefold), and icu.Locale object.
+#   Return string either casefolded or converted to appropriate case.
+def casing(text, case="lower", loc=None):
+    if loc == None:
+        loc = ROOT_LOCALE
+    case = case.lower()
+    if case == "lower":
+        text = str(UnicodeString(text).toLower(loc))
+    elif case == "upper":
+        text = str(UnicodeString(text).toUpper(loc))
+    elif case == "title":
+        text = str(UnicodeString(text).toTitle(loc))
+    elif case == "casefold" or case == "fold":
+        text = str(UnicodeString(text).foldCase(loc))
+    return text
