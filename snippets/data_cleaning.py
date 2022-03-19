ff2d431ed5e2ca428021b83ff6b7bff2c79be96a
Andj <andjc@users.noreply.github.com>
2022-03-20 02:55:51 +1100

    Update data_cleaning.py

--- snippets/data_cleaning.py
+++ snippets/data_cleaning.py
@@ -6,7 +6,30 @@
 #
 ####################
 
+# import unicodedata as ud
+import unicodedataplus as ud
 import regex as re
+
+
+#
+# Unicode normalisation
+#   Simple wrappers for Unicode normalisation
+#
+
+
+def NFD(s):
+    return ud.normalize('NFD', s)
+
+def NFKD(s):
+    return ud.normalize('NFKD', s)
+
+def NFC(s):
+    return ud.normalize('NFC', s)
+
+def NFKC(s):
+    return ud.normalize('NFKC', s)
+
+
 
 #
 # Clean presentation forms
