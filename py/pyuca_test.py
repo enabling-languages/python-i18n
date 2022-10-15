8aaacba598740b46936097b5f9fc7e04cd246821
Andj <andjc@users.noreply.github.com>
2022-10-15 17:16:08 +1100

    tweaks

--- py/pyuca_test.py
+++ py/pyuca_test.py
@@ -1,9 +1,7 @@
 import pyuca
 test_list = ["₨", "Z", "ز", "z", "ر", "٨", "R", "﷼"]
-
 ducet_rules = "../rules/collation/allkeys_DUCET.txt"
 cldr_rules = "../rules/collation/allkeys_CLDR.txt"
-
 ducet_collator = pyuca.Collator(ducet_rules)
 cldr_collator = pyuca.Collator(cldr_rules)
 
